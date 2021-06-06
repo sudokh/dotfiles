beef80db02d2779cd91ceaebd896cd69f9e1b859
SUDO Kohei <sdukuhi.520@gmail.com>
2021-06-06 14:33:09 +0900

    improve install function

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-#!bin/bash
+#!/bin/bash
 
 function lnfile () {
-  if [ -e $1 ]; then
-    echo "$1 already exists."
+  if [ -e ~/$1 ]; then
+    echo "~/$1 already exists."
   else
-    ln -s $HOME/.dotfiles/$1 $HOME/$1
+    ln -s $SCRIPT_DIR/$1 ~/$1
     echo "$1 is installed !"
   fi
 }
 
+SCRIPT_DIR=`pwd -P`
+lnfile .vimrc
 lnfile .zshrc
 lnfile .tmux.conf
-
-source $HOME/.zshrc
 
